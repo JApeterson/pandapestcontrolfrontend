7c5d9fd3cde86dcc67c52d0a861f6b64e50e1a96
Joseph Peterson <japeter89@gmail.com>
2021-10-22 14:00:13 -0700

    now responsive to images

--- src/modules/views/ProductHero.js
+++ src/modules/views/ProductHero.js
@@ -2,73 +2,88 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@mui/styles';
 import Button from '../components/Button';
-import ProductHeroLayout from './ProductHeroLayout';
 import { Link } from "react-router-dom";
 import { Typography, Grid } from '@mui/material';
-// import Carousel from 'react-material-ui-carousel'
+import Carousel from 'react-material-ui-carousel'
 
-//const images = ['./public/slideshow1.jpg', './slideshow2.jpg', './slideshow3.jpg', './slideshow4.jpg', './slideshow5.jpg', './slideshow6.jpg']
-// const backgroundImage =
-//  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80';
+const images = ['slideshow1.jpg', 'slideshow3.jpg', 'slideshow4.jpg']
 const styles = (theme) => ({
-  background: {
-    backgroundImage: `url('slideshow1.jpg')`,
-    backgroundColor: '#7fc7d9', // Average color of the background image.
-    backgroundPosition: 'center',
-  },
-  h5: {
-    marginBottom: theme.spacing(4),
-    marginTop: theme.spacing(4),
+
+  text: {
+    position: 'absolute',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    left: 0,
+    right: 0,
+    textAlign: 'center', zIndex: 1,
+    [theme.breakpoints.up('xs')]: {
+      fontSize: 32
+    },
     [theme.breakpoints.up('sm')]: {
-      marginTop: theme.spacing(10),
+      fontSize: 64,
+      lineHeight: '100%',
+      marginTop: '30px'
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: 72
+    },
+
+  },
+  image: {
+    [theme.breakpoints.down('md')]: {
+      height: '100%',
+      width: '100%'
     },
   },
-  more: {
-    marginTop: theme.spacing(2),
+  button: {
+    position: 'absolute',
+    zIndex: 1,
+    top: 450,
+
+    [theme.breakpoints.down('md')]: {
+      top: 300,
+    },
+    [theme.breakpoints.down('sm')]: {
+      top: 250,
+    },
   },
 });
 
 function ProductHero(props) {
   const { classes } = props;
-
   return (
-    <ProductHeroLayout backgroundClassName={classes.background}>
-      <Grid container justifyContent="center" spacing={12}>
-        {/* <Carousel>
-        {
-          images.map((path, i) => <img style={{ display: 'none' }} src={path} alt="Panda workers doing their job." />)
-        }
-      </Carousel> */}
-        <Grid item xs={12}>
-          <img style={{ display: 'none' }} src={'./public/slideshow1.jpg'} alt="Panda workers doing their job." />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography color="inherit" variant="h2" align='center' >
-            <span style={{ color: "#5bd620" }}>{'Clean'}</span> {'pest control, for your lifestyle'}
-          </Typography>
-        </Grid>
-        <Grid item xs={8} md={3}>
-          <Link to={"/quote"}>
-            <Button
-              fullWidth
-              style={{ backgroundColor: '#5bd620', marginTop: 60 }}
-              variant="contained"
-              size="large"
-              component="a"
-            >
-              Get Pricing
-            </Button>
-          </Link>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography align='center'>Panda Pest Control is Arizona's top rated eco-friendly pest control service. With over a decade of pest control experience, our team of pest control professionals are trained and equipped to protect your home or business. Using cutting edge technology along with best green pest control products, Panda Pest Control is able to deliver the best results.
-            Although we are best known for our success rate in scorpion control; our five step regular protection program is what has disrupted the pest control industry in Arizona.Find out why we are Arizona's premier pest control company.
-          </Typography>
-          <br />
-          <br />
-        </Grid>
+    <Grid container justifyContent="center">
+      <Grid item xs={12}>
+        <text className={classes.text} >
+          <span style={{ color: "#5bd620" }}>{'Clean'}</span> {'pest control, for your lifestyle'}
+        </text>
+        <Carousel indicators={false} animation='slide' stopAutoPlayOnHover={false}>
+
+          {
+            images.map((path, i) => <img className={classes.image} src={path} alt="Panda workers doing their job." />)
+          }
+        </Carousel>
       </Grid>
-    </ProductHeroLayout >
+      <Grid className={classes.button} item xs={8} md={3} >
+        <Link to={"/quote"}>
+          <Button
+
+            fullWidth
+            style={{ backgroundColor: '#5bd620' }}
+            variant="contained"
+            size="large"
+            component="a"
+          >
+            Get Pricing
+          </Button>
+        </Link>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography style={{ margin: '20px', marginTop: 60 }} align='center'>Panda Pest Control is Arizona's top rated eco-friendly pest control service. With over a decade of pest control experience, our team of pest control professionals are trained and equipped to protect your home or business. Using cutting edge technology along with best green pest control products, Panda Pest Control is able to deliver the best results.
+          Although we are best known for our success rate in scorpion control; our five step regular protection program is what has disrupted the pest control industry in Arizona.Find out why we are Arizona's premier pest control company.
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
 
